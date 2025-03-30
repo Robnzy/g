ce80753c1e3802883f1d969ab90de021cd8c7fb9
Robnzy <Robnzy@gmail.com>
2025-03-30 22:26:18 +0300

    Update true.php

--- gift/true.php
+++ gift/true.php
@@ -1,18 +1,13 @@
 <?php
-// قائمة الأسماء الصحيحة
-$valid_names = "سارة", "ساره", "سارتي", "سوستي","سوسة","Sara","Sarah";
+// قائمة الأسماء الصحيحة كمصفوفة
+$valid_names = array("سارة", "ساره", "سارتي", "سوستي", "سوسة", "Sara", "Sarah");
 
-// التحقق من إرسال البيانات عن طريق POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // الحصول على الاسم المدخل
     $name = $_POST['name'];
-
-    // التحقق من صحة الاسم
+    
     if (in_array($name, $valid_names)) {
-        // إذا كان الاسم صحيحًا، إعادة توجيه إلى صفحة success.php
         header("Location: true.html");
     } else {
-        // إذا كان الاسم خطأ، إعادة توجيه إلى صفحة error.php
         header("Location: false.html");
     }
     exit();
